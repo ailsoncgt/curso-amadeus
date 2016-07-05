423a4adf0908153da1ada471ebd1d888f908297b
Thiago Araújo <thiagosoara@gmail.com>
2016-07-05 17:52:12 -0300

    adicionando herança

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return render(request, "index.html")
+
+def contact(request):
+	return render(request, "contact.html")
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from core.views import index
+from core import views
 
 
 urlpatterns = [
-	url(r'^$', index),
+	url(r'^$', views.index),
+	url(r'^contato/$', views.contact),
     url(r'^admin/', admin.site.urls),
 ]
